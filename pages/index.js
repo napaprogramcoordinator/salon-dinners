cfbb414a90f2348a823d0cbac475388efcd574d0
napaprogramcoordinator <napaprogramcoordinator@gmail.com>
2026-01-10 19:33:30 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Calendar, Users, CheckCircle, AlertCircle, X, Edit, Clock, Mail, Trash2 } from 'lucide-react';
-import { supabase } from './supabaseClient';
+import { supabase } from '../lib/supabaseClient';
 
 const SalonDinners = () => {
   const [currentPage, setCurrentPage] = useState('public');
