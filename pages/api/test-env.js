d0e01c27bb01a5ad44483c627005f670b4e6507a
napaprogramcoordinator <napaprogramcoordinator@gmail.com>
2026-01-12 11:11:55 -0500

    Update test-env.js

--- pages/api/test-env.js
+++ pages/api/test-env.js
@@ -1,7 +1,3 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    hasEmail: !!process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    hasKey: !!process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY,
-    hasSheetId: !!process.env.GOOGLE_SPREADSHEET_ID
-  });
-}
+git add pages/api/test-env.js
+   git commit -m "Better env var testing"
+   git push origin main
